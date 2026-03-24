4a347c6e871af4271e61fcb1862f2cc3d9e25a7c
Koki Fushimi <paalon1936@gmail.com>
2026-03-25 05:16:54 +0900

    Split the subroutine `play` to `say` and `play` in Modern Fortran implementation.

--- impl/fortran/fizzbuzz.f90
+++ impl/fortran/fizzbuzz.f90
@@ -9,18 +9,24 @@ subroutine play(n)
   implicit none
   integer, intent(in) :: n
   integer :: i
-  logical :: is_fizz, is_buzz
   do i = 1, n
-    is_fizz = mod(i, 3) == 0
-    is_buzz = mod(i, 5) == 0
-    if (is_fizz .and. is_buzz) then
-      print '(A)', 'fizz buzz'
-    else if (is_fizz) then
-      print '(A)', 'fizz'
-    else if (is_buzz) then
-      print '(A)', 'buzz'
-    else
-      print '(I0)', i
-    end if
+    call say(i)
   end do
 end subroutine play
+
+subroutine say(n)
+  implicit none
+  integer, intent(in) :: n
+  logical :: is_fizz, is_buzz
+  is_fizz = mod(n, 3) == 0
+  is_buzz = mod(n, 5) == 0
+  if (is_fizz .and. is_buzz) then
+    print '(A)', 'fizz buzz'
+  else if (is_fizz) then
+    print '(A)', 'fizz'
+  else if (is_buzz) then
+    print '(A)', 'buzz'
+  else
+    print '(I0)', n
+  end if
+end subroutine say
